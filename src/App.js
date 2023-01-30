8997e3d927ba37f19e1557cbbbb1fc0115d341ee
Bennacer Akram <km_bennacer@esi.dz>
2023-01-30 03:17:03 +0100

    SearchBar state done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Search from './pages/search/search';
-import NiveauIcon from './components/gradHat';
-import React, { useState } from 'react';
-
 
 function App() {
-
   return (
     <Search></Search>
-    );
+  );
 }
 
 export default App;
--- src/pages/search/search.jsx
+++ src/pages/search/search.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Filters from "./filters";
 import Card from "../../components/course_card";
 import { FiSearch } from "react-icons/fi";
 
+
 const Search = (props) => {
+    const [searchInput, setSearchInput] = useState('');
+    const onChangeSearch = (e) => { 
+        setSearchInput(e.target.value);
+        console.log(searchInput);
+    }
+
     return ( 
         <div className="flex justify-center bg-myLightGreen pb-60 pt-11">
                 <Filters/>
@@ -12,7 +19,7 @@ const Search = (props) => {
                         <span class="absolute inset-y-0 right-[18px] flex items-center pl-2">
                             <FiSearch className="text-[24px] text-iconGray"></FiSearch>
                         </span>
-                        <input class=" w-full h-[50px] py-2 pl-[20px] rounded-[8px] shadow-md focus:outline-none placeholder:text-iconGray placeholder:font-publicSans placeholder:font-Regular placeholder:text-lg font-publicSans font-Regular text-lg" placeholder="Que voulez-vous apprendre ?" type="text" name="search"/>
+                        <input class=" w-full h-[50px] py-2 pl-[20px] rounded-[8px] shadow-md focus:outline-none placeholder:text-iconGray placeholder:font-publicSans placeholder:font-Regular placeholder:text-lg font-publicSans font-Regular text-lg" placeholder="Que voulez-vous apprendre ?" type="text" name="search" onChange={onChangeSearch}/>
                     </label>
                     <div className="grid grid-cols-3 gap-[24px] mt-[24px] w-full ">
                         <Card/>
@@ -26,9 +33,8 @@ const Search = (props) => {
                         <Card/>
                     </div>
                 </div>
-           
         </div>
-     );
+    );
 }
  
 export default Search;
